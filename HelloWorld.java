a7895bd7508559f4c42753ecf635d1c780715935
Srinivas <adityasrinivasb@gmail.com>
2016-05-08 17:20:55 +0530

    Added helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+package inheritanceDemo;
+
+public class HelloWorld {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello, Welcome to Jenkins");
+
+	}
+
+}
+
+//sample1
+//sample2
+//sample3
+//sample4
+//sample5
+
+
+
